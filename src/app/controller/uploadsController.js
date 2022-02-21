1d037c49d96f652371303e8f63746d155611e5f0
Ossman O. C <ocarvajalt@slabcode.com>
2022-02-21 08:39:28 -0500

    Lista la parte para subir archivos

--- src/app/controller/uploadsController.js
+++ src/app/controller/uploadsController.js
@@ -1,57 +1,7 @@
 let multer = require('multer')
-let files = require('../middlewares/fileMiddleware')
+let fileUpload = require('../middlewares/fileMiddleware')
 
 module.exports = {
-
-    index: function (req, res) {
-        let courses = [ 
-            {id_course: 1, course_name: "Ciência da Computação"},
-            {id_course: 1, course_name: "Ciência da Computação"}
-        ]
-
-        console.log(courses)
-        res.render("index", courses)
-    },
-    list: function (req, res) {
-        files.get(req.con,
-            function (err, rows) {
-                if (err) console.error(err)
-                console.table(rows)
-                res.render("files", { files: rows })
-            })
-    },
-
-    create: function (req, res) {
-        console.log(req.body)
-        files.create(req.con, req.body.file_name,
-            function (err, rows) {
-                if (err) console.error(err)
-                console.table(rows)
-                res.redirect("/files")
-            })
-    },
-
-    edit: function (req, res) {
-        const id = req.params.id
-        const name = req.body;
-
-        files.update(req.con, id, name,
-            function (err, rows) {
-                if (err) console.error(err)
-                console.table(rows)
-                res.redirect("/files")
-            })
-    },
-
-    delete: function (req, res) {
-        console.log(req.params)
-        files.delete(req.con, req.params.id,
-            function (err, rows) {
-                if (err) console.error(err)
-                console.table(rows)
-                res.redirect("/files")
-            })
-    },
 
     fileUploadForm: function (req, res) {
         res.render('upload-form');
